e8dadd1004554a45248fb0c97f2dd3e71b05c457
marcos luciano <marcos3dt@gmail.com>
2023-03-28 01:24:56 -0300

    ajustes do codigo

--- src/main/java/Usuario.java
+++ src/main/java/Usuario.java
@@ -5,33 +5,28 @@ import javax.xml.transform.stream.StreamSource;
 
 public class Usuario {
     public static void main(String[] args) {
-
+        System.out.println("==================================================");
        SmartTV smartTV = new SmartTV();
-
        smartTV.mudaCanal(7);
        smartTV.aumentaCanal();
        smartTV.aumentaCanal();
        smartTV.aumentaCanal();
-
        smartTV.diminuiCanal();
        System.out.println("Canal: " + smartTV.canal);
-
        smartTV.aumentarVolume();
        smartTV.aumentarVolume();
        smartTV.aumentarVolume();
        smartTV.aumentarVolume();
        smartTV.aumentarVolume();
-        System.out.println("Volume Atual ? " + smartTV.volume);
-
-//        System.out.println("Tv ligada ? " + smartTV.ligada);
-//        System.out.println("Canal Atual ? " + smartTV.canal);
-//        System.out.println("Volume Atual ? " + smartTV.volume);
-//
-//        smartTV.ligar();
-//        System.out.println("novo Tv ligada ? " + smartTV.ligada);
-//
-//        smartTV.ligada();
-//        System.out.println("novo Tv ligada ? " + smartTV.ligada);
+       System.out.println("Volume Atual ? " + smartTV.volume);
+       System.out.println("Tv ligada ? " + smartTV.ligada);
+       System.out.println("Canal Atual ? " + smartTV.canal);
+       System.out.println("Volume Atual ? " + smartTV.volume);
+       smartTV.ligar();
+       System.out.println("novo Tv ligada ? " + smartTV.ligada);
+       smartTV.ligada();
+       System.out.println("novo Tv ligada ? " + smartTV.ligada);
+       System.out.println("==================================================");
     }
     }
 
